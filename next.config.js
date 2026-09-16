4e20a6ed5a04c00bfeff29bd0c25ba4bfc014810
Red Pymes <clientesredpymes@gmail.com>
2026-09-15 22:25:29 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -42,6 +42,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "pub-194989ec07984c729e39ce5c034a9961.r2.dev",
+      },
       ...(S3_HOSTNAME && S3_PATHNAME
         ? [
             {
